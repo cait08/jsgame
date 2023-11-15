19bd12b37fe0457c1ca3e42e20809c49dc21f67a
cait b <cait@mdibl.org>
2023-11-15 18:51:04 -0500

    got rid of 2d stuff

--- src/game/game.ts
+++ src/game/game.ts
@@ -1,7 +1,4 @@
 import { Subject } from "rxjs";
-import { Critter } from "./critter";
-import { Entity } from "./entity";
-import { Background } from "./background";
 import { Geom } from "./geom";
 import { Dice } from "./dice";
 import { Drawing } from "./drawing";
@@ -9,9 +6,6 @@ import { Input } from "./input";
 import { Physics } from "./physics";
 
 export class Game {
-  canvas: HTMLCanvasElement;
-  ctx: CanvasRenderingContext2D;
-
   drawing: Drawing;
 
   tick = new Subject<void>();
@@ -19,8 +13,6 @@ export class Game {
   geom = new Geom();
 
   nextGameTick = new Date().getTime();
-
-  entities: Entity[] = [];
 
   paused = false;
 
@@ -35,71 +27,10 @@ export class Game {
     screen.height = params.height;
     screen.width = params.width;
 
-    this.canvas = screen;
     const ctx = screen.getContext("2d");
     if (!ctx) {
       return;
     }
-    this.ctx = ctx;
-
-    this.drawing = new Drawing(this.ctx);
-    document.body.append(div);
-
-    div.appendChild(this.canvas);
-    this.render();
-    this.think();
-
-    this.canvas.addEventListener("click", (evt: MouseEvent) => {
-      for (const e of this.entities) {
-        e.faceAngle([evt.x, evt.y]);
-        e.think();
-      }
-
-      this.addEntity(
-        new Critter({
-          position: [evt.x, evt.y],
-          color: "red",
-          angle: 90,
-        })
-      );
-    });
-  }
-
-  entitiesInReach(entity: Entity) {
-    const x = entity.x;
-    const y = entity.y;
-    let radius = 20;
-    if (typeof (entity as any).radius === "number") {
-      radius = (entity as any).radius;
-    }
-
-    return this.entities.filter(
-      (e) =>
-        e.x > x - radius &&
-        e.x < x + radius &&
-        e.x > y - radius &&
-        e.y < y + radius
-    );
-  }
-
-  addEntity(item: Entity) {
-    if (this.entities.indexOf(item) > -1) {
-      return;
-    }
-
-    item.ctx = this.ctx;
-    item.tick = this.tick;
-    item.game = this;
-    item.geom = this.geom;
-
-    item.drawing = new Drawing(this.ctx);
-    item.drawing;
-
-    this.entities.push(item);
-  }
-
-  removeEntity(item: Entity) {
-    this.entities.splice(this.entities.indexOf(item), 1);
   }
 
   think() {
@@ -109,43 +40,8 @@ export class Game {
       }, 100);
     }
 
-    for (const e of this.entities) {
-      e.think();
-    }
-
     setTimeout(() => {
       this.think();
     }, 24);
-  }
-
-  render() {
-    setTimeout(() => {
-      requestAnimationFrame(() => {
-        if (!this.paused) {
-          this.ctx.fillStyle = "#eeeeee";
-          this.ctx.strokeStyle = "#000000";
-
-          this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-
-          if (Dice.roll(1)) {
-            this.entities.sort((a, b) => a.z - b.z);
-          }
-
-          for (const e of this.entities) {
-            e.draw();
-          }
-
-          this.drawing.text(
-            [700, 20],
-            this.entities.length.toString(),
-            "black"
-          );
-        }
-
-        //this.ctx.scale(1, 2); // squish it to 2/3 vertical size
-        this.render();
-        this.frame++;
-      });
-    }, 16);
   }
 }
